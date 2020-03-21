1c6ba6ff49cae27b3e5fc95431e93cc265a63cb6
Maevabzn <44022913+Maevabzn@users.noreply.github.com>
2020-03-21 18:34:44 +0100

    third commit

--- scripts/clipboard.js
+++ scripts/clipboard.js
@@ -1,9 +1,26 @@
+console.log('coucou')
+
 if (navigator.clipboard) {
   document.querySelectorAll('[data-clipboard]').forEach(($clipboardEl) => {
     const $button = document.createElement('button');
     $button.innerHTML = 'Copier';
     $clipboardEl.parentNode.append($button);
+
+    $button.addEventListener(
+      'click',
+      copyToClipboard.bind(this, $clipboardEl, $button),
+    );
   });
 } else {
   console.log('Presse papier non supporté :(');
+}
+
+function copyToClipboard($clipboardEl, $button) {
+  navigator.clipboard
+    .writeText($clipboardEl.getAttribute('data-clipboard'))
+    .then(() => {
+      $button.innerHTML = 'Copié !';
+      setTimeout(() => ($button.innerHTML = 'Copier'), 2000);
+    })
+    .catch((err) => console.warn(err));
 }
---
+++ sw.js
@@ -0,0 +1,42 @@
+addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('offline').then((cache) => {
+      cache.add('offline.html');
+    }),
+  );
+});
+
+console.log('je suis fatiguée')
+
+addEventListener('fetch', (event) => {
+  if (event.request.headers.get('Accept').includes('text/html')) {
+    console.log('j en ai marre')
+    event.respondWith(
+      fetch(event.request)
+        .then((res) => {
+          if (isPartyPage(event.request.url)) {
+            const copy = res.clone();
+            caches
+              .open('parties')
+              .then((cache) => cache.put(event.request, copy));
+            return res;
+          } else {
+            return res;
+          }
+        })
+        .catch(() => {
+          if (isPartyPage(event.request.url)) {
+            return caches
+              .match(event.request)
+              .catch(() => caches.match('offline.html'));
+          } else {
+            return caches.match('offline.html');
+          }
+        }),
+    );
+  }
+});
+
+function isPartyPage(url) {
+  return /party\/[a-zA-Z0-9]*$/.test(url);
+}
